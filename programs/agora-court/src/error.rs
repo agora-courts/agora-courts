2a4c7734df0861a4cca0aef95a3565a943a9db5a
Javier Nieto <javiermnieto89@gmail.com>
2022-11-11 18:19:01 -0600

    init errors

---
+++ programs/agora-court/src/error.rs
@@ -0,0 +1,19 @@
+use anchor_lang::error_code;
+
+#[error_code]
+pub enum InputError {
+    #[msg("Invalid court authority")]
+    InvalidCourtAuthority,
+    #[msg("Dispute does not contain payer")]
+    DisputeDoesNotContainPayer,
+    #[msg("User does not have a case in this dispute")]
+    UserDoesNotHaveCase,
+    #[msg("Dispute is not currently votable")]
+    DisputeNotVotable,
+    #[msg("Dispute is not finalizable")]
+    DisputeNotFinalizable,
+    #[msg("Evidence already submitted")]
+    EvidenceAlreadySubmitted,
+    #[msg("Cases already submitted")]
+    CasesAlreadySubmitted,
+}
