d16ac581a353d2ed007cc5e5d5e2a438e0d76061
kevinz420 <kzhang0305@gmail.com>
2022-11-17 21:50:40 -0600

    reputation state

--- programs/agora-court/src/state/court.rs
+++ programs/agora-court/src/state/court.rs
@@ -3,11 +3,34 @@ use anchor_lang::prelude::*;
 
 #[account]
 pub struct Court {
-    pub authority: Pubkey,
-    pub num_disputes: u64,
-    pub bump: u8,
+    minWindow: u32, // 4 bytes
+    minArbCost: u64, // 8 bytes
+    lastDID: u64 // 8 bytes
 }
 
 impl Court {
-    pub const SIZE: usize = DISCRIMINATOR_SIZE + 32 + 8 + 1;
+    // discriminator - 8 bytes
+    pub const STATIC_SIZE: usize = 8 + 4 + 8 + 8;
+    
+    pub fn initialize(&mut self, _minWindow: u32, _minArbCost: u64) -> Result<()> {
+        self.lastDID = 0;
+        self.minWindow = _minWindow;
+        self.minArbCost = _minArbCost;
+        Ok(())
+    }
+
+    pub fn set_window(&mut self, _minWindow: u32) -> Result<()> {
+        self.minWindow = _minWindow;
+        Ok(())
+    }
+
+    pub fn set_min_cost(&mut self, _minArbCost: u64) -> Result<()> {
+        self.minArbCost = _minArbCost;
+        Ok(())
+    }
+
+    pub fn inc_did(&mut self) -> Result<()> {
+        self.lastDID += 1;
+        Ok(())
+    }
 }
