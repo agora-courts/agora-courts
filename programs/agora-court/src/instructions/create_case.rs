b6e64fb64e8d0d93070ec6951fcc618f2ca210c3
Javier Nieto <javiermnieto89@gmail.com>
2022-11-21 17:00:26 -0600

    Fix create case context

--- programs/agora-court/src/instructions/create_case.rs
+++ programs/agora-court/src/instructions/create_case.rs
@@ -2,19 +2,35 @@ use crate::{error::InputError, state::*};
 use anchor_lang::prelude::*;
 
 #[derive(Accounts)]
-#[instruction(evidence: String)]
+#[instruction(dispute_id: u64, evidence: String)]
 pub struct CreateCase<'info> {
     #[account(
         init,
-        seeds = [b"case".as_ref(), dispute.key().as_ref(), payer.key().as_ref()],
+        seeds = [b"case".as_ref(), court.key().as_ref(), dispute.key().as_ref(), payer.key().as_ref()],
         bump,
         payer = payer,
-        space = Case::get_space(evidence)
+        space = Case::get_size(evidence)
     )]
     pub case: Account<'info, Case>,
 
-    #[account(mut)]
+    #[account(
+        mut,
+        seeds = [b"dispute".as_ref(), court.key().as_ref(), u64::to_ne_bytes(dispute_id).as_ref()],
+        constraint = dispute.users.contains(&payer.key())
+                    @ InputError::UserDoesNotHaveCase,
+        bump = dispute.bump,
+    )]
     pub dispute: Account<'info, Dispute>,
+
+    #[account(
+        mut,
+        seeds = [b"court".as_ref(), court_authority.key().as_ref()],
+        bump = court.bump,
+    )]
+    pub court: Account<'info, Court>,
+
+    /// CHECK: Creator of court.
+    pub court_authority: UncheckedAccount<'info>,
 
     #[account(mut)]
     pub payer: Signer<'info>,
@@ -22,22 +38,23 @@ pub struct CreateCase<'info> {
     pub system_program: Program<'info, System>,
 }
 
-pub fn create_case(ctx: Context<CreateDispute>, evidence: String) -> Result<()> {
-    let case = &mut ctx.accounts.case;
+pub fn create_case(ctx: Context<CreateCase>, _dispute_id: u64, evidence: String) -> Result<()> {
     let dispute = &mut ctx.accounts.dispute;
-    case.initialize(dispute.key(), evidence);
-
     require!(
         dispute.status == DisputeStatus::Waiting,
-        InputError::EvidenceAlreadySubmitted
+        InputError::CasesAlreadySubmitted
     );
-
-    require!(
-        dispute.users.contains(&user),
-        InputError::UserDoesNotHaveCase
-    );
-
-    dispute.case_submitted();
+    let case = &mut ctx.accounts.case;
+    let bump = *ctx.bumps.get("case").unwrap();
+    case.set_inner(Case {
+        votes: 0,
+        evidence,
+        bump,
+    });
+    dispute.submitted_cases += 1;
+    if dispute.submitted_cases == dispute.users.len() {
+        dispute.status = DisputeStatus::Voting;
+    }
 
     Ok(())
 }
