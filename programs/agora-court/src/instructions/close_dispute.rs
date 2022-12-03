edae775f2c9d39357ee1073abd3580e0be545ed3
kevinz420 <kzhang0305@gmail.com>
2022-12-02 23:09:56 -0600

    bruh

--- programs/agora-court/src/instructions/close_dispute.rs
+++ programs/agora-court/src/instructions/close_dispute.rs
@@ -1,5 +1,5 @@
 /*
-    1. Check that ends_at blocktime has passed OR abstain votes / all votes = target %
+    1. Check that ends_at blocktime has passed
     2. Change dispute status accordingly
     3. Return status
 */
